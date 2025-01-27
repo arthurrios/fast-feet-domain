dc385a859347231bfc7be1dd9be6d346314cf080
arthur <arthur.rios007@gmail.com>
2025-01-27 16:29:45 -0300

    feat: make user class extendable

--- src/domain/user/enterprise/entities/user.ts
+++ src/domain/user/enterprise/entities/user.ts
@@ -1,17 +1,25 @@
 import { Entity } from '@/core/entities/entity'
 import { Role } from '../../@types/role'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Optional } from '@/core/types/optional'
 
 export interface UserProps {
   name: string
+  cpf: string
   email: string
   password: string
   role: Role
+  createdAt: Date
+  updatedAt?: Date | null
 }
 
-export class User extends Entity<UserProps> {
+export class User<Props extends UserProps> extends Entity<Props> {
   get name() {
     return this.props.name
+  }
+
+  get cpf() {
+    return this.props.cpf
   }
 
   get email() {
@@ -22,12 +30,26 @@ export class User extends Entity<UserProps> {
     return this.props.password
   }
 
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
   get role() {
     return this.props.role
   }
 
-  static create(props: UserProps, id?: UniqueEntityID) {
-    const user = new User(props, id)
+  static create(props: Optional<UserProps, 'createdAt'>, id?: UniqueEntityID) {
+    const user = new User(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    )
 
     return user
   }
