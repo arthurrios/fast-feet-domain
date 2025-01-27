7c7a6bb333821be806dfae8d864672e9ef7aee9f
arthur <arthur.rios007@gmail.com>
2025-01-27 13:20:31 -0300

    feat: add order class

---
+++ src/domain/delivery/enterprise/entities/order.ts
@@ -0,0 +1,93 @@
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Slug } from './value-objects/slug'
+import { OrderStatus } from '../../@types/status'
+import { Address } from './value-objects/address'
+import { Optional } from '@/core/types/optional'
+
+export interface OrderProps {
+  courierId: UniqueEntityID
+  recipientId: UniqueEntityID
+  title: string
+  description: string
+  slug: Slug
+  address: Address
+  status: OrderStatus
+  createdAt: Date
+  updatedAt?: Date | null
+}
+
+export class Order extends Entity<OrderProps> {
+  get courierId() {
+    return this.props.courierId
+  }
+
+  get recipientId() {
+    return this.props.recipientId
+  }
+
+  get title() {
+    return this.props.title
+  }
+
+  get description() {
+    return this.props.description
+  }
+
+  get slug() {
+    return this.props.slug
+  }
+
+  get address() {
+    return this.props.address
+  }
+
+  get status() {
+    return this.props.status
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  set title(title: string) {
+    this.props.title = title
+    this.props.slug = Slug.createFromText(title)
+    this.touch()
+  }
+
+  set description(description: string) {
+    this.props.description = description
+    this.touch()
+  }
+
+  set status(status: OrderStatus) {
+    this.props.status = status
+    this.touch()
+  }
+
+  static create(
+    props: Optional<OrderProps, 'createdAt' | 'slug'>,
+    id?: UniqueEntityID,
+  ) {
+    const order = new Order(
+      {
+        ...props,
+        slug: props.slug ?? Slug.createFromText(props.title),
+        status: props.status ?? OrderStatus.AWAITING_COURIER,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    )
+
+    return order
+  }
+}
