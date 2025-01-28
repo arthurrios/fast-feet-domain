2ebdff756c13a726b7786a31170352e66a593752
arthur <arthur.rios007@gmail.com>
2025-01-27 21:37:11 -0300

    feat: update courier and order entities

--- src/domain/delivery/enterprise/entities/courier.ts
+++ src/domain/delivery/enterprise/entities/courier.ts
@@ -2,30 +2,54 @@ import { User, UserProps } from '@/domain/user/enterprise/entities/user'
 import { CourierOrderList } from './courier-order-list'
 import { Address } from './value-objects/address'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Role } from '@/domain/user/@types/role'
 
-export interface CourierProps extends UserProps {
+export interface CourierProps {
   orders: CourierOrderList
   address: Address
 }
 
-export class Courier extends User<CourierProps> {
+export class Courier {
+  private _user: User
+  private ordersList: CourierOrderList
+  private addressDetails: Address
+
+  constructor(
+    user: User,
+    ordersList: CourierOrderList,
+    addressDetails: Address,
+  ) {
+    this._user = user
+    this.ordersList = ordersList
+    this.addressDetails = addressDetails
+  }
+
   get orders() {
-    return this.props.orders
+    return this.ordersList
   }
 
   get address() {
-    return this.props.address
+    return this.addressDetails
   }
 
-  static create(props: CourierProps, id?: UniqueEntityID) {
-    const courier = new Courier(
+  get user() {
+    return this._user
+  }
+
+  static create(
+    props: CourierProps,
+    userProps: UserProps,
+    id?: UniqueEntityID,
+  ): Courier {
+    const user = User.create(
       {
-        ...props,
-        orders: props.orders ?? new CourierOrderList(),
+        ...userProps,
+        role: Role.COURIER,
+        createdAt: new Date(),
       },
       id,
     )
 
-    return courier
+    return new Courier(user, props.orders, props.address)
   }
 }
--- src/domain/delivery/enterprise/entities/order.ts
+++ src/domain/delivery/enterprise/entities/order.ts
@@ -6,8 +6,8 @@ import { Address } from './value-objects/address'
 import { Optional } from '@/core/types/optional'
 
 export interface OrderProps {
-  courierId: UniqueEntityID
-  recipientId: UniqueEntityID
+  courierId?: UniqueEntityID | null
+  recipientId?: UniqueEntityID
   title: string
   description: string
   slug: Slug
